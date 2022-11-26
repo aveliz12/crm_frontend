21577c26a9fa75ccb31359a271c4f1b76ab422ad
aveliz12 <alexpaulveliz12@gmail.com>
2022-11-26 10:45:05 -0500

    Creacion de formulario Login y registro

--- pages/newAccount.jsx
+++ pages/newAccount.jsx
@@ -1,11 +1,108 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const newAccount = () => {
+  //Validacion de formularion
+  const formik = useFormik({
+    initialValues: {
+      //valores iniciales que van en el campo de cada input
+      name: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (valores) => {
+      console.log("enviando..");
+      console.log(valores);
+    },
+  });
+
   return (
     <>
       <Layout>
-        <h1>Nuevo Registro</h1>
+        <h1 className="text-center text-2xl text-white font-light">
+          CREAR NUEVA CUENTA
+        </h1>
+        <div className="flex justify-center mt-5">
+          <div className="w-full max-w-sm">
+            <form
+              className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+              onSubmit={formik.handleSubmit}
+            >
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="name"
+                >
+                  Nombre:
+                </label>
+                <input
+                  className="bg-white shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="name"
+                  type="text"
+                  placeholder="Nombre de Usuario"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="lastName"
+                >
+                  Apellido:
+                </label>
+                <input
+                  className="bg-white shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="lastName"
+                  type="text"
+                  placeholder="Apellido del Usuario"
+                  value={formik.values.lastName}
+                  onChange={formik.handleChange}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email:
+                </label>
+                <input
+                  className="bg-white shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Email Usuario"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password:
+                </label>
+                <input
+                  className="bg-white shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Password Usuario"
+                  values={formik.values.password}
+                  onChange={formik.handleChange}
+                />
+              </div>
+              <input
+                type="submit"
+                className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900"
+                value="Crear cuenta"
+              />
+            </form>
+          </div>
+        </div>
       </Layout>
     </>
   );
