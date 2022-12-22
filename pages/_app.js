9822c54cbffd80c0211f13245529549528ed2dca
aveliz12 <alexpaulveliz12@gmail.com>
2022-12-21 20:07:20 -0500

    Commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,7 @@ import { ApolloProvider } from "@apollo/client";
 import client from "../config/apollo";
 
 const MyApp = ({ Component, pageProps }) => {
-
   
-
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
--- pages/login.jsx
+++ pages/login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <>
       <Layout>
-        <h1 className="text-center text-2xl text-white font-light">LOGIN</h1>
+        <h1 className="text-center text-2xl text-white font-light">BIENVENIDO AL LOGIN</h1>
         <div className="flex justify-center mt-5">
           <div className="w-full max-w-sm">
             <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
